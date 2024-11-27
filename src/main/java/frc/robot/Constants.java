23eaf6f67eb38089067e9cb711e1576c74db6a2a
Étienne <cote04a1@eleve.csrs.qc.ca>
2024-11-27 18:19:18 -0500

    Made climber subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,4 +5,12 @@
 package frc.robot;
 
 /** Add your docs here. */
-public final class Constants {}
+public final class Constants {
+  public final class ClimberConstants {
+    /** Motor Id for left climber (placeholder value rn because idk what it is) */
+    public static int kLeftClimberId = 0;
+
+    /** Motor Id for left climber (placeholder value rn because idk what it is) */
+    public static int kRightClimberId = 0;
+  }
+}
