1e9a72f47ba420d30576d42a1c4b813289a84580
Étienne <cote04a1@eleve.csrs.qc.ca>
2024-12-02 18:47:59 -0500

    Finished config (i think)

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,28 +4,39 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Climber extends SubsystemBase {
+  /** Follows climber_right */
   private CANSparkMax m_climber_left =
       new CANSparkMax(Constants.ClimberConstants.kLeftClimberId, MotorType.kBrushless);
+
   private CANSparkMax m_climber_right =
       new CANSparkMax(Constants.ClimberConstants.kRightClimberId, MotorType.kBrushless);
 
+  private double rampRate = 1.5;
+
   /** Creates a new Climber. */
   public Climber() {
-    // i dont even know if and how im supposed to do this but too bad im tryinig something anyway
     m_climber_left.restoreFactoryDefaults();
     m_climber_right.restoreFactoryDefaults();
 
     m_climber_left.follow(m_climber_right, true);
+
+    m_climber_left.setOpenLoopRampRate(rampRate);
+    m_climber_right.setOpenLoopRampRate(rampRate);
+
+    m_climber_left.setIdleMode(IdleMode.kBrake);
+    m_climber_right.setIdleMode(IdleMode.kBrake);
+
+    m_climber_left.burnFlash();
+    m_climber_right.burnFlash();
   }
 
   @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+  public void periodic() {}
 }
