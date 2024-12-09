1524224b041451cf122de93f8109b9462483b91e
simonjub <78098752+simonjub@users.noreply.github.com>
2024-12-09 17:43:33 -0500

    more WIP

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,11 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
+// import frc.robot.subsystems.drive.Drive;
+
 public class RobotContainer {
+  // private final Drive drive;
+
   public RobotContainer() {
     configureBindings();
   }
